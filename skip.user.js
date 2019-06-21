e7c02561cc3e9a8c013c2e51bafe2705e68db0ad
largereptile <harry@yark.org.uk>
2019-06-21 13:20:45 +0100

    now removes button when you click it

--- skip.user.js
+++ skip.user.js
@@ -7,15 +7,33 @@
 // @version     1
 // ==/UserScript==
 
-var div = document.getElementsByClassName("controls")[0].children[0];
-var but = document.createElement("button");
-but.className = "strong";
-but.id = "openingskip";
-but.innerHTML = "Skip Op";
-
 var toSkip = 89; // not 90 because i have poor reaction time
 
-but.onclick = function skipOpening() {
-    document.getElementsByClassName("AT-player")[0].children[0].currentTime += toSkip;
+function addButton() {
+    var div = document.getElementsByClassName("controls")[0].children[0];
+  
+	  var but = document.createElement("button");
+    but.className = "strong";
+    but.id = "openingskip";
+    but.innerHTML = "Skip Op";
+
+    but.onclick = function skipOpening() {
+        document.getElementsByClassName("AT-player")[0].children[0].currentTime += toSkip;
+        var x = document.getElementById("openingskip");
+        x.parentNode.removeChild(x);
+    }
+    
+    div.insertBefore(but, div.firstChild);
 }
-div.insertBefore(but, div.firstChild);
+
+addButton();
+
+var buttons = document.querySelectorAll('[role="button"]'); 
+
+for (var i = 0; i < buttons.length; i++) {
+  
+    buttons[i].addEventListener("click", addButton);
+  
+}
+
+document.querySelector(".countdown > button:first-of-type").addEventListener("click", addButton);
